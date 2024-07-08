50cd6ab79bf50a663b9d13103d0315e06027c9e9
FEAROFTHEQ <devadevam.tod@gmail.com>
2024-07-08 13:38:49 +0300

    finaly

--- src/main.js
+++ src/main.js
@@ -1,6 +1,38 @@
-import './js/about';
-import './js/projects';
-import './js/faq';
-import './js/reviews';
-import './js/footer';
-import './js/header-modal';
+// import './js/about';
+// import './js/projects';
+// import './js/faq';
+// import './js/reviews';
+// import './js/footer';
+// import './js/header-modal';
+
+const scroller = document.querySelector(".marquee");
+
+if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches){
+    addAnimation();
+    console.log("HEREFDSFSDF");
+}
+function addAnimation(){
+    scroller.setAttribute('data-animated', true);
+    const scrollerInner = scroller.querySelectorAll(".marquee-inner");
+    const scrollerInnerArray =  Array.from(scrollerInner);
+    scrollerInnerArray.forEach((scroller) =>{
+        const scrollerContent = Array.from(scroller.children);
+        scrollerContent.forEach(item => {
+            const duplItem = item.cloneNode(true);
+            duplItem.setAttribute('aria-hidden', true);
+            scroller.classList.add('duplicated');
+            scroller.appendChild(duplItem);
+            console.log(duplItem);
+        });
+    });
+
+
+    // const scrollerInner = scroller.querySelector(".marquee-inner");
+    // const scrollerContent = Array.from(scrollerInner.children);
+    // scrollerContent.forEach(item => {
+    //     const duplItem = item.cloneNode(true);
+    //     duplItem.setAttribute('aria-hidden', true);
+    //     scrollerInner.appendChild(duplItem);
+    //     console.log(duplItem);
+    // })
+}
